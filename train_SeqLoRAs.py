b067ffbbc4ffde457ad6e5db7271d6f9451a28fa
nikhil-chigali <nikhil.chigali@gmail.com>
2024-07-04 09:59:46 +0530

    Setup sanity check

--- train_SeqLoRAs.py
+++ train_SeqLoRAs.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 from argparse import ArgumentParser
 import torch
 
@@ -28,16 +29,8 @@ from src.models import SequentialLoRAModel
 """
 
 
-def main(exp_name):
+def main(exp_name: str, tasks: List[str]):
     config = get_seq_lora_config(exp_name, "qv")
-    config = add_task_to_seq_lora_config(config, "sst2")
-
-    # Create the experiment directory
-    os.makedirs(f"experiments/{exp_name}/checkpoints", exist_ok=True)
-    os.makedirs(f"experiments/{exp_name}/logs", exist_ok=True)
-    os.makedirs("state_dicts/stacked_loras", exist_ok=True)
-
-    logger.info(f"Running experiment: {exp_name}")
 
     # Set the seed
     seed_everything(config.seed)
@@ -45,21 +38,15 @@ def main(exp_name):
     # Set the float32 matmul precision
     torch.set_float32_matmul_precision("high")
 
-    # Data module
-    logger.info("Preparing data module")
-    data_module_cls = get_datamodule(config.tasks[0])
-    data_module = data_module_cls(
-        model_name=config.model.model_name,
-        batch_size=config.training.batch_size,
-        num_workers=config.data[0].num_workers,
-    )
-    data_module.prepare_data()
-    data_module.setup()
+    # Create the experiment directory
+    os.makedirs(f"experiments/{exp_name}/checkpoints", exist_ok=True)
+    os.makedirs(f"experiments/{exp_name}/logs", exist_ok=True)
+    os.makedirs("state_dicts/seq-loras", exist_ok=True)
 
-    # Model
-    logger.info("Creating model")
-    model = SequentialLoRAModel(config)
+    logger.info(f"Running experiment: {exp_name}")
 
+    model = None
+    data_modules = {}
     # Callbacks
     callbacks = [
         ModelCheckpoint(
@@ -76,40 +63,82 @@ def main(exp_name):
         ),
     ]
 
-    # Loggers
-    loggers = [
-        CSVLogger(f"experiments/{exp_name}/logs/"),
-        WandbLogger(
-            project="LoRA-Ensembling",
-            name=exp_name,
-            log_model=True,
-            save_dir="experiments",
-        ),
-    ]
+    for i, task in enumerate(tasks):
+        # Adding task to Config
+        logger.info(f"Adding task - `{task}` to config dict")
+        if model is None:
+            config = add_task_to_seq_lora_config(config, task)
+        else:
+            model.config = add_task_to_seq_lora_config(model.config, task)
+            config = model.config
 
-    # Trainer
-    logger.info("Creating trainer")
-    trainer = Trainer(
-        max_epochs=config.training.max_epochs,
-        accelerator=config.training.accelerator,
-        precision=config.training.precision,
-        deterministic=True,
-        val_check_interval=config.training.val_check_interval,
-        callbacks=callbacks,
-        logger=loggers,
-    )
+        # Data module
+        logger.info(f"Preparing data module - `{task}`")
+        data_module_cls = get_datamodule(task)
+        data_modules[task] = data_module_cls(
+            model_name=config.model.model_name,
+            batch_size=config.training.batch_size,
+            num_workers=config.data[i].num_workers,
+        )
+        data_modules[task].prepare_data()
+        data_modules[task].setup()
 
-    # Train the model
-    logger.info("Training model")
-    trainer.fit(model, datamodule=data_module)
+        # Model
+        if model is None:
+            logger.info("Creating model")
+            model = SequentialLoRAModel(config)
+        else:
+            model.add_task(task)
+            model.set_curr_task(task)
 
-    # logger.info("Training complete. Saving final model state dict")
-    # model.save_artifacts(trainer)
+        # Loggers
+        loggers = [
+            CSVLogger(f"experiments/{exp_name}/logs/"),
+            WandbLogger(
+                project="LoRA-Ensembling",
+                name=f"exp_name-{task}",
+                log_model=True,
+                save_dir="experiments",
+            ),
+        ]
+
+        # Trainer
+        logger.info("Creating trainer")
+        trainer = Trainer(
+            max_epochs=config.training.max_epochs,
+            accelerator=config.training.accelerator,
+            precision=config.training.precision,
+            deterministic=True,
+            val_check_interval=config.training.val_check_interval,
+            callbacks=callbacks,
+            logger=loggers,
+        )
+
+        # Save the init model weights
+        model.save_lora_stack(
+            f"state_dicts/seq-loras/loras_r{model.config.lora.r}_{'-'.join(tasks)}_INIT.pt"
+        )
+        model.save_classifier(
+            f"state_dicts/seq-loras/cls_r{model.config.lora.r}_{'-'.join(tasks)}_INIT.pt"
+        )
+
+        # Train the model
+        logger.info("Training model")
+        trainer.fit(model, datamodule=data_modules[task])
+
+        # Save the model
+        model.save_lora_stack(
+            f"state_dicts/seq-loras/loras_r{model.config.lora.r}_{'-'.join(tasks)}.pt"
+        )
+        model.save_classifier(
+            f"state_dicts/seq-loras/cls_r{model.config.lora.r}_{'-'.join(tasks)}.pt"
+        )
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument("--exp_name", type=str, required=True)
+    arg_parser.add_argument("--tasks", type=str, required=True)
     args = arg_parser.parse_args()
 
-    main(args.exp_name)
+    main(args.exp_name, args.tasks.split("-"))
